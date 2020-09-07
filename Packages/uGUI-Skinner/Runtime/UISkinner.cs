60201bcff67fcd4ff4c24e0ce4ec424f356ed154
pspkurara <itinomiyakurara@yahoo.co.jp>
2020-09-08 00:09:11 +0900

    docs: コメント追加

--- Packages/uGUI-Skinner/Runtime/UISkinner.cs
+++ Packages/uGUI-Skinner/Runtime/UISkinner.cs
@@ -13,10 +13,20 @@ namespace Pspkurara.UI
 		[SerializeField] private int m_StyleIndex = 0;
 		[SerializeField] private List<SkinStyle> m_Styles = null;
 
+		/// <summary>
+		/// 全てのスキン
+		/// </summary>
 		public List<SkinStyle> styles { get { return m_Styles; } }
 
+		/// <summary>
+		/// 現在のスキンのスタイル番号
+		/// </summary>
 		public int styleIndex { get { return m_StyleIndex; } }
-		
+
+		/// <summary>
+		/// スキンを切り替える
+		/// </summary>
+		/// <param name="styleIndex">スキンのスタイル番号</param>
 		public void SetSkin(int styleIndex)
 		{
 			m_StyleIndex = Mathf.Clamp(styleIndex, 0, Length);
@@ -24,12 +34,19 @@ namespace Pspkurara.UI
 			m_Styles[m_StyleIndex].Apply();
 		}
 
+		/// <summary>
+		/// スキンを切り替える
+		/// </summary>
+		/// <param name="styleKey">スキンのスタイルキー</param>
 		public void SetSkin(string styleKey)
 		{
 			int index = m_Styles.FindIndex(s => s.styleKey == styleKey);
 			if (index != -1) SetSkin(index);
 		}
 
+		/// <summary>
+		/// スキンの長さ
+		/// </summary>
 		public int Length {
 			get { return m_Styles.Count; }
 		}
